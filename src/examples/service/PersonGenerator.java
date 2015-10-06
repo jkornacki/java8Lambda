95779ac4bff171541d77fded33f560f72aca5e59
Jacek Kornacki <jacek.kornacki@agora.pl>
2015-10-06 12:39:08 +0200

    - Literowki

--- src/examples/service/PersonGenerator.java
+++ src/examples/service/PersonGenerator.java
@@ -5,9 +5,6 @@ import domain.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by P on 2015-10-05.
- */
 public class PersonGenerator {
 
     public List<Person> generateExamplePerson() {
@@ -16,11 +13,11 @@ public class PersonGenerator {
         persons.add(new Person("Jakub","Kowalczyk",21));
         persons.add(new Person("Piotr","Dudek",20));
         persons.add(new Person("Maciej","Zawadzki",20));
-        persons.add(new Person("Julian","Jab³oñski",20));
+        persons.add(new Person("Julian","Jaboobski",20));
         persons.add(new Person("Julia","Baran",40));
-        persons.add(new Person("Marta","B¹k",10));
+        persons.add(new Person("Marta","Bok",10));
         persons.add(new Person("Daria","Michalska",35));
-        persons.add(new Person("Jêdrzej","Urbañski",61));
+        persons.add(new Person("Jedrzej","Urbanski",61));
         persons.add(new Person("Anna","Mazur",22));
         return persons;
     }
